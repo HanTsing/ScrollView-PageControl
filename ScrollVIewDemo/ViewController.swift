0dd96dda067fcc4b06c9b04733d3e0b6250de65f
hanqing <tsinghan07@gmail.com>
2015-02-26 22:29:18 +0800

    finish

--- ScrollVIewDemo/ViewController.swift
+++ ScrollVIewDemo/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    var screenWidth  = 0
+    var screenHeight = 0
+    
+    var images = ["tutorial_background_00","tutorial_background_01","tutorial_background_02","tutorial_background_03","tutorial_background_04"]
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        scrollView.delegate = self
+        screenWidth  = Int(self.view.frame.width)
+        screenHeight = Int(self.view.frame.height)
+        
+        scrollView.contentSize = CGSize(width: screenWidth*5, height: screenHeight)
+        pageControl.addTarget(self, action: "pageChanged", forControlEvents: UIControlEvents.ValueChanged)
+        
+        for(var i=0; i<5; i++){
+            var image = UIImageView(frame: CGRect(x: i*screenWidth, y: 0, width: screenWidth, height: screenHeight))
 
+            image.image = UIImage(named: images[i])
+            scrollView.addSubview(image)
+        }
+        
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func scrollViewDidScroll(scrollView: UIScrollView){
+        var currentPage = Int(scrollView.contentOffset.x/320)
+        pageControl.currentPage = Int(currentPage)
+    }
+    
+    func pageChanged(){
+        var currentPage = pageControl.currentPage
+        scrollView.scrollRectToVisible(CGRect(x: currentPage*screenWidth, y: 0, width: screenWidth, height: screenHeight), animated: true)
+    }
+    
+    
 }
 
